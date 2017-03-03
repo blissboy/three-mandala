94b92e103246fb3487127a7f555b51bfcbe014c2
Pat Wheaton <pat@onprem.com>
2017-03-03 17:06:16 -0600

    WIP does not work

--- mandala.js
+++ mandala.js
@@ -15,6 +15,8 @@ function init() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
+    var bubble_radius = 10;
+
     createScene();
 }
 
@@ -38,6 +40,10 @@ function createGeometries() {
             new THREE.MeshLambertMaterial({ color: 0xfd59d7 })
         )
     );
+
+    let squiggles = createSquiggleTubes(createSquiggles());
+
+
 }
 
 function updateGeometries() {
@@ -68,3 +74,134 @@ function updateCamera() {
 
 }
 
+/**
+ * Do walk until the function passed says that the point we have reached is out of bounds. For example, 
+ * could pass a function like the following to determine if the point was outside the drawing window:
+ *  (currentPointOfWalk) => {
+ *      if ( currentPointOfWalk.x > window.innerWidth 
+ *          || currentPointOfWalk.x < 0 
+ *          || currentPointOfWalk.y > window.innerHeight 
+ *          || currentPointOfWalk.y < 0 ) {
+ *          return false;
+ *      } else {
+ *          return true;
+ *      }
+ * }
+ * 
+ * @param startingPoint the point from which to start the walk
+ * @param isPointStillInRegion a function that determines if the walk is still within the region. For example, 
+ * could pass a function like the following to determine if the point was outside the drawing window:
+ *  (currentPointOfWalk) => {
+ *      if ( currentPointOfWalk.x > window.innerWidth 
+ *          || currentPointOfWalk.x < 0 
+ *          || currentPointOfWalk.y > window.innerHeight 
+ *          || currentPointOfWalk.y < 0 ) {
+ *          return false;
+ *      } else {
+ *          return true;
+ *      }
+ * }
+ * @param startingVector the direction that the path is facing at the start of the process
+ * @param calculateNextStep function that will calculate the next step given the current location 
+ * and array of steps as an input. 
+ * 
+ * @returns an array of steps (in the form {x,y,z}) indicating the steps. 
+ */
+function getStepsAcrossRegion( 
+    startingPoint, 
+    isPointStillInRegion, 
+    startingVector,
+    calculateNextStep = (location, steps) => {return {x: random(), y: random(), z:random()};} ) {
+    
+    steps = [];
+    let currentPoint = startingPoint;
+    do {
+        steps.push(calculateNextStep(currentLocation, steps)); 
+        currentPoint = {
+            x: currentPoint.x + steps[steps.length - 1].x,
+            y: currentPoint.y + steps[steps.length - 1].y,
+        };
+    } while (isPointStillInRegion(currentPoint))
+
+    return steps;
+}
+
+/**
+ * create paths across the bubble
+ * 
+ * @returns array of squiggle lines
+ */
+function createSquiggles() {
+    return [];
+}
+
+function createSquiggleTubes(squiggleLines) {
+    return [];
+}
+
+function createSquiggle() {
+{\displaystyle {\begin{aligned}x&=r\,\sin \theta \,\cos \varphi \\y&=r\,\sin \theta \,\sin \varphi \\z&=r\,\cos \theta \end{aligned}}}
+    //herehere - need to create squiggle from one point on sphere and then the draw should put that everywhere. 
+    let phi, theta;
+    const twoPI = Math.PI * 2.0;
+    for ( i=1; i<=longitudePoints; i++) {
+        for (j=1; j<=latitudePoints; j++) {
+            // get surface normal
+            theta = twoPI / i;
+            phi = twoPI / j;
+            x = bubble_radius * Math.sin(theta) * Math.cos(phi);
+            y = bubble_radius * Math.sin(theta) * Math.sin(phi);
+            z = bubble_radius * Math.cos(theta);
+            
+        }
+    }
+
+    let steeps = getStepsAcrossRegion();
+    return createBeziersBetweenSteps(steeps);
+    //return createLineBetweenSteps(steeps);
+}
+
+function createBeziersBetweenSteps(steeps) {
+    return (x, y) => {
+        console.log(x,y);
+        let curPtX = x;
+        let curPtY = y;
+        context.beginPath();
+        context.lineWidth = "1";
+        //context.strokeStyle = "green";
+        context.strokeStyle = getMovingRainbowXGradient(0, widdth);
+        context.moveTo(curPtX, curPtY);
+        for (i=0; i<steeps.length - 2; i++) {
+            curPtX += steeps[i].x;
+            curPtY += steeps[i].y;
+            context.bezierCurveTo(
+                curPtX + steeps[i+1].x, curPtY + steeps[i+1].y,
+                curPtX + steeps[i+2].x, curPtY + steeps[i+2].y,
+                curPtX, curPtY);
+        }
+        context.stroke();
+        context.closePath();
+    }
+}
+
+
+function createLineBetweenSteps(steeps) {
+    return (x, y) => {
+
+        context.moveTo(0, 0);
+        let curPtX = x;
+        let curPtY = y;
+        context.beginPath();
+        context.lineWidth = "1";
+        //context.strokeStyle = "green";
+        context.strokeStyle = getMovingRainbowXGradient(0, widdth);
+        context.moveTo(curPtX, curPtY);
+        steeps.forEach(step => {
+            curPtX += step.x;
+            curPtY += step.y;
+            context.lineTo(curPtX, curPtY);
+        });
+        context.stroke();
+        context.closePath();
+    }
+}
